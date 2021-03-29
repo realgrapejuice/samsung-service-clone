b9d3c31a4b779e7e210fe06a3eb6395264052d37
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-03-29 17:30:47 +0900

    사이트맵 CSS

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// 글로벌 변수 섹션
+const navUl = document.querySelector(".nav__list");
+
+// 함수 섹션
+const handleNavUlMouseover = (event) => {};
+
+// 이벤트 핸들러 섹션
+navUl.addEventListener("mouseover", () => {
+  console.log("hello");
+});
