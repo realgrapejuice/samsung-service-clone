7c7f134cd4df635e6f8d47875a0a657d8ec548f9
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-03-29 18:36:55 +0900

    사이트맵 섹션에 자바스크립트 코드를 추가해 간단한 인터랙션이 가능하도록 구현함

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 // 글로벌 변수 섹션
+// Nav Variables
 const navUl = document.querySelector(".nav__list");
 
+// Sitemap Variables
+const sitemapBlock = document.querySelector(".header__sitemap");
+
 // 함수 섹션
-const handleNavUlMouseover = (event) => {};
+const handleNavUlMouseover = (event) => {
+  sitemapBlock.style.display = "flex";
+};
+
+const handleNavUlMouseoverMouseleave = (event) => {
+  sitemapBlock.style.display = "none";
+};
 
 // 이벤트 핸들러 섹션
-navUl.addEventListener("mouseover", () => {
-  console.log("hello");
-});
+navUl.addEventListener("mouseover", handleNavUlMouseover);
+
+sitemapBlock.addEventListener("mouseleave", handleNavUlMouseoverMouseleave);
